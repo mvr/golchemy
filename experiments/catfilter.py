d3accf68cbcadafb56cb46bced4e092aac5c5d67
mvr <mitchell.v.riley@gmail.com>
2022-07-19 07:48:44 +1000

    Try to make filtering more useful

--- experiments/catfilter.py
+++ experiments/catfilter.py
@@ -1,4 +1,5 @@
 import sys
+from enum import Enum
 
 from golchemy.lab import *
 
@@ -13,27 +14,32 @@ correctiontransform = "identity"
 maxtime = 150
 
 standard_reagents = [
-    book.reagents['r'],
-    book.reagents['b'],
-    book.reagents['century'],
-    book.reagents['dove'],
-    book.reagents['e'],
-    # book.reagents['glider'],
-    book.reagents['herschel'],
-    book.reagents['i'],
+    'r',
+    'b',
+    'century',
+    'dove',
+    'e',
+    # 'glider',
+    'herschel',
+    # 'herschelprime',
+    'i',
 
-    book.reagents['blonktie'],
-    book.reagents['pi'],
-    book.reagents['queenbee'],
-    book.reagents['r'],
-    book.reagents['wing'],
-    book.reagents['lom'],
-    book.reagents['uturner'],
-    book.reagents['honeyfarmer'],
-    book.reagents['lwss'],
-    book.reagents['mwss'],
-    book.reagents['hwss'],
+    'blonktie',
+    'pi',
+    'queenbee',
+    'r',
+    'wing',
+    'lom',
+    'uturner',
+    'honeyfarmer',
+    'lwss',
+    'mwss',
+    'hwss',
+
+    'octomino',
 ]
+book.reagents['lwss'].notable = True
+
 
 ptbfile = sys.argv[1]
 
@@ -94,61 +100,170 @@ def interpret_pattern(p):
         catalysts.pattern -= a.pattern
     return tr.inverse() * s, tr.inverse()*catalysts
 
+
+class Reason(Enum):
+    SINGLETON = 0
+    SHOOTING = 1
+    STANDARD = 2
+    SURVIVING = 3
+    SPECIAL = 4
+    TRANSPARENT = 5
+    GLIDER = 6
+
 def is_interesting(s, catalysts):
-    gliders = len([r for r in s.reagents if r.reagent.name == 'glider'])
+    gliders = [r for r in s.reagents if r.reagent.name == 'glider']
 
-    if len(s.reagents) - len(catalysts.reagents) - gliders == 0:
-        return True
-    if len(s.reagents) - len(catalysts.reagents) - gliders <= 2 \
-       and all(i.trans.offset.taxicab_length >= 20 for i in s.reagents if not i in catalysts.reagents):
-        return True
-    if len(s.reagents) - len(catalysts.reagents) - gliders <= 3 \
-       and any(i.reagent in standard_reagents for i in s.reagents):
-        return True
+    # for g in gliders:
+    #     if g.trans.lin == LinearTransform.flip_x or g.trans.lin == LinearTransform.rot270:
+    #         return Reason.GLIDER
 
-    return False
+    if len(s.chaos) == 0 and len(s.reagents) - len(catalysts.reagents) - len(gliders) <= 1:
+        return Reason.SINGLETON
 
-lineno = 0
-rles = {}
+    if len(s.chaos) == 0 and len(s.reagents) - len(catalysts.reagents) - len(gliders) <= 3 \
+       and any(i.reagent.name in standard_reagents for i in s.reagents):
+        return Reason.STANDARD
+    if any(i.reagent.name in standard_reagents and i.time > 30 for i in s.reagents):
+        return Reason.SURVIVING
+    if any(i.reagent.notable and not i in catalysts.reagents for i in s.reagents):
+        return Reason.SPECIAL
+
+    # if any(i.reagent.name in standard_reagents
+    #        and i.trans.offset.taxicab_length > 25
+    #        and i.time > 20
+    #        for i in s.reagents):
+    #     return Reason.SHOOTING
+
+    # nonactives = [r for r in s.reagents if not r.reagent.is_active]
+    activepart = s.copy()
+    for g in gliders:
+        activepart.reagents.remove(g)
+        activepart.pattern -= g.pattern
+
+    activebb = activepart.pattern.bb
+    limit = 30
+
+    if abs(activebb.x) > limit or \
+       abs(activebb.x + activebb.w) > limit or \
+       abs(activebb.y) > limit or \
+       abs(activebb.y + activebb.h) > limit:
+        return Reason.SHOOTING
+
+    return None
+
+
+goodpats = {}
 data = {}
 seen = []
-for p in get_patterns():
-    s, catalysts = interpret_pattern(p)
-    if not catalysts:
-        continue
-    orig = s.copy()
+def go():
+    lastreason = None
+    for p in get_patterns(ptbfile):
+        s, catalysts = interpret_pattern(p)
 
-    if (lineno % 100) == 0:
-        print(f"Doing line {lineno}")
+        if not catalysts:
+            continue
+        orig = s.copy()
+        catalystsborder = catalysts.pattern.zoi() - catalysts.pattern
 
-    hasinteracted = False
-    isinteresting = False
-    for i in range(0, maxtime):
-        s, es = s.step(book)
-        s.normalise()
+        # if any(not i.reagent.name in boring_catalysts for i in catalysts.reagents):
+        #     activedigest = catalysts.pattern.digest()
+        #     output = [str(i) for i in catalysts.reagents]
+        #     print(orig.pattern.rle_string_only, 0, Reason.SPECIAL.name, output)
+        #     rles[activedigest] = [orig.pattern.rle_string_only]
+        #     data[activedigest] = (0, Reason.SPECIAL.name, output)
 
-        if len(s.chaos) > 0:
-            hasinteracted = True
-            isinteresting = False
+        hasinteracted = False
+        lastinteresting = None
+        hasdestroyed = False
+        destroyedrs = []
+        hasreportedtrans = False
+        reasons = set()
+        for i in range(0, maxtime):
+            if len(s.chaos) > 0:
+                hasinteracted = True
+            if not lastinteresting is None and lastinteresting != [i.reagent.name for i in s.reagents]:
+                lastinteresting = None
 
-        if hasinteracted \
-           and not isinteresting \
-           and len(s.chaos) == 0 \
-           and is_interesting(s, catalysts) \
-           and (catalysts.pattern - s.pattern).population == 0:
-            isinteresting = True
-            activedigest = (s.pattern - catalysts.pattern).digest()
-            if activedigest in seen:
-                rles[activedigest].append(orig.pattern.rle_string_only)
-                break
-            seen.append(activedigest)
-            output = [str(i) for i in s.reagents if not i in catalysts.reagents]
-            print(orig.pattern.rle_string_only, i, output)
-            rles[activedigest] = [orig.pattern.rle_string_only]
-            data[activedigest] = (i, output)
-    lineno += 1
+            for c in catalysts.reagents:
+                if c in destroyedrs:
+                    continue
+                if c.pattern.population < 10 and (c.pattern - s.pattern).population >= 4:
+                    destroyedrs.append(c)
+                    hasdestroyed = True
 
+            reportable = hasinteracted \
+               and (catalysts.pattern - s.pattern).empty() \
+               and (catalystsborder & s.pattern).empty()
+
+            shouldreport = reportable \
+               and not (isinteresting := is_interesting(s, catalysts)) is None \
+               and (lastinteresting is None or isinteresting != lastreason) \
+               and not isinteresting in reasons
+
+            if reportable and hasdestroyed and not hasreportedtrans:
+                shouldreport = True
+                isinteresting = Reason.TRANSPARENT
+                hasreportedtrans = True
+
+            if shouldreport:
+                lastinteresting = [i.reagent.name for i in s.reagents]
+                lastreason = isinteresting
+                if isinteresting != Reason.STANDARD:
+                    reasons.add(isinteresting)
+
+                activedigest = (s.pattern - catalysts.pattern).digest()
+                if isinteresting == Reason.TRANSPARENT:
+                    destroyedpat = lt.pattern()
+                    destroyedpat[-200,-200] = 1 # stupid hack to stop lifelib from shifting everything to the origin
+                    for c in destroyedrs:
+                        destroyedpat += c.pattern
+                    activedigest = destroyedpat.digest()
+
+                if activedigest in seen:
+                    goodpats[activedigest].append(orig.pattern)
+                    continue
+
+                seen.append(activedigest)
+                output = [str(i) for i in s.reagents if not i in catalysts.reagents]
+                if len(s.chaos) > 0:
+                    output += ["CHAOS"]
+                if isinteresting == Reason.TRANSPARENT:
+                    output = [str(i) for i in destroyedrs]
+                print(orig.pattern.rle_string_only, i, isinteresting.name, output)
+                goodpats[activedigest] = [orig.pattern]
+                data[activedigest] = (i, isinteresting.name, output)
+
+            s, es = s.step(book)
+            s.normalise()
+
+# import cProfile
+# cProfile.run("go()", "profileout")
+
+go()
+
+print("writing summary")
 pp = pprint.PrettyPrinter(indent=2)
 with open(ptbfile+'filtered', 'w') as f:
-    result = [(rles[s], data[s][0], data[s][1]) for s in seen]
+    result = [([p.rle_string_only for p in goodpats[s]], data[s][0], data[s][1], data[s][2]) for s in seen]
     f.write(f"{pp.pformat(result)}")
+
+print("writing groups")
+total = lt.pattern()
+j = 0
+for s in seen:
+    i = 0
+    for p in goodpats[s]:
+        total += p.shift(i*100 + 50, j*100 + 50)
+        i += 1
+    j += 1
+
+with open(ptbfile+'groups', 'w') as f:
+    f.write(total.rle_string())
+
+# import pstats
+# from pstats import SortKey
+
+# with open("profileout.txt", "w") as f:
+#     ps = pstats.Stats("profileout", stream=f)
+#     ps.sort_stats(SortKey.CUMULATIVE)
+#     ps.print_stats()
