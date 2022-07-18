84eed08f59152ffa949763cd12e90e15a31dd49d
mvr <mitchell.v.riley@gmail.com>
2022-07-19 07:39:54 +1000

    More convenient identify script

--- experiments/identify.py
+++ experiments/identify.py
@@ -1,13 +1,34 @@
 import sys
 
-from golchemy.lab import *
+from os import path
+import pickle
+
+from golchemy.basics import *
+from golchemy.schematic import *
+
+if path.isfile("bookgiant.pickle"):
+    with open(f"bookgiant.pickle", 'rb') as f:
+        book = pickle.load(f)
+
+else:
+    reagentfiles = ["reagents/ash.toml", "reagents/constellations.toml", "reagents/commonsmall.toml", "reagents/commonactive.toml", "reagents/notabledebris.toml", "reagents/sevencell.toml",  "reagents/catalysts.toml", "reagents/smallcensusall.toml",]
+    book = Book.from_toml_object(toml.load(reagentfiles))
+    with open(f"bookgiant.pickle", 'wb') as f:
+         pickle.dump(book, f)
+
 
 rle = sys.argv[1]
 
 p = lt.pattern(rle)
+last = "xxx"
+print(rle)
 for t in range(1,100):
     if p in book:
         r = book[p]
-        print(book[p])
-        exit()
+        if not r.reagent.name == last:
+            print(f"Becomes {book[p]} at time {t}")
+            last = r.reagent.name
     p = p.advance(1)
+
+if last == "xxx":
+    print("Unknown")
